d929a2d93da096e12d2576d7c3a1b30e01900d3d
Kerry Nicholson <kerrynic@mit.edu>
2014-01-17 15:45:11 -0500

    During the nav test

--- user/robot/umain.c
+++ user/robot/umain.c
@@ -5,8 +5,6 @@
 #define ANGLE_FACTOR 651.898647649
 
 uint8_t team_number[2] = {1,0};
-extern volatile uint8_t robot_id;
-
 
 extern volatile uint8_t robot_id; 
 
@@ -28,25 +26,32 @@ int limitVelocity(int velocity) {
 }
 
 int turn_to_heading(void){
+	
 	copy_objects();
-	float kp= .11;
+	float kp= 0.06;
 	float desired_heading=atan((game.coords[1].y-game.coords[0].y)/(game.coords[1].x-game.coords[0].x));
 	if (game.coords[1].y>game.coords[0].y && desired_heading<0){//quadrant 2
 		desired_heading+=PI;}
-	else if (game.coords[1].y<game.coords[0].y && desired_heading>0){//quadrant 4
+	else if (game.coords[1].y<game.coords[0].y && desired_heading>0){//quadrant 3
 		desired_heading-=PI;}
 	desired_heading*=ANGLE_FACTOR;
+	//printf("desired heading %.3f\n", desired_heading);
 	int actual_heading=game.coords[0].theta;
-	float error_angle = desired_heading-actual_heading;
-	int controller_output= (int)kp*error_angle;
-	
-	motor_set_vel(1, limitVelocity(controller_output));
-    return ((desired_heading - actual_heading) > 40);
+	//printf("actual heading %d\n", actual_heading);
+	float error_angle = desired_heading-actual_heading;	
+	//printf("error_angle %.3f\n",error_angle);
+	int controller_output;
+	if (abs(error_angle)<10){
+		controller_output=0;}
+	else controller_output= (int)(kp*error_angle);
+	//printf("controller_output: %d\n",controller_output);
+	motor_set_vel(5, limitVelocity(controller_output));	
+    return ((desired_heading - actual_heading) > 20);
 }
 
 int straight_to_heading(void){
 	copy_objects();
-	int kp = 0.01;
+	float kp = 0.12;
 	float x_now = game.coords[0].x;
 	float y_now = game.coords[0].y;
 	float x_new = game.coords[1].x;
@@ -54,15 +59,19 @@ int straight_to_heading(void){
 	float x_diff_squared = pow((x_new-x_now),2);
 	float y_diff_squared = pow((y_new-y_now),2);
 	float error_distance = sqrt(x_diff_squared + y_diff_squared);
-	int error_velocity = (int) kp*error_distance;
-		
-	motor_set_vel(0,limitVelocity(error_velocity));
+	int error_velocity = (int) (kp*error_distance);
+	//printf("error_velocity: %d\n",error_velocity);
+	motor_set_vel(4,limitVelocity(error_velocity));
     return (error_distance < 10);
 }
 
 void reorient_and_drive(void){
+	copy_objects();
+	printf("%d is x_new, %d is y_new, %d is x_goal, %d is y_goal, %d is current_heading",game.coords[0].x,game.coords[0].y,game.coords[1].x,game.coords[1].y,game.coords[0].theta);
+	
     while (turn_to_heading()){      //Turn first
     }
+	printf("%d is x_new, %d is y_new, %d is x_goal, %d is y_goal, %d is current_heading",game.coords[0].x,game.coords[0].y,game.coords[1].x,game.coords[1].y,game.coords[0].theta);
 	while(1) {                      //Then go straight, with corrections if necessary
     straight_to_heading();
     turn_to_heading();
