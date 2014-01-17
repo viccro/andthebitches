57108604c02814438e8f26e79b0dff0550812e08
Vicki Crosson <vicki.crosson@gmail.com>
2014-01-17 06:22:32 -0500

    Updated umain with "reorient_and_drive"

--- user/robot/umain.c
+++ user/robot/umain.c
@@ -11,22 +11,22 @@ extern volatile uint8_t robot_id;
 extern volatile uint8_t robot_id; 
 
 // usetup is called during the calibration period. 
->
+
 int usetup (void) {
 	robot_id = 1;
     return 0;
 }
-int limitVelocity(int velocity) {
 
+int limitVelocity(int velocity) {
 	int speed = abs(velocity);
 	if(speed > 255) 
-		return 255*(velocity/speed)
+		return 255*(velocity/speed);
 	else if (speed < 0)
 		return 0;
 	else
 		return velocity;
-
 }
+
 int turn_to_heading(void){
 	copy_objects();
 	float kp= .11;
@@ -37,15 +37,16 @@ int turn_to_heading(void){
 		desired_heading-=PI;}
 	desired_heading*=ANGLE_FACTOR;
 	int actual_heading=game.coords[0].theta;
-	int error = desired_heading-actual_heading;
-	float controller_output= (int)kp*error;
+	float error_angle = desired_heading-actual_heading;
+	int controller_output= (int)kp*error_angle;
 	
 	motor_set_vel(1, limitVelocity(controller_output));
-	return 0;
+    return ((desired_heading - actual_heading) > 40);
 }
+
 int straight_to_heading(void){
 	copy_objects();
-	int kp = 0.01
+	int kp = 0.01;
 	float x_now = game.coords[0].x;
 	float y_now = game.coords[0].y;
 	float x_new = game.coords[1].x;
@@ -53,19 +54,30 @@ int straight_to_heading(void){
 	float x_diff_squared = pow((x_new-x_now),2);
 	float y_diff_squared = pow((y_new-y_now),2);
 	float error_distance = sqrt(x_diff_squared + y_diff_squared);
-	int error_velocity = (int) kp*error_distance
+	int error_velocity = (int) kp*error_distance;
 		
-	motor_set_vel(0,limitVelocity(error_velocity))
+	motor_set_vel(0,limitVelocity(error_velocity));
+    return (error_distance < 10);
 }
+
+void reorient_and_drive(void){
+    while (turn_to_heading()){      //Turn first
+    }
+	while(1) {                      //Then go straight, with corrections if necessary
+    straight_to_heading();
+    turn_to_heading();
+	}
+}
+
 
 // Entry point to contestant code.
 int umain (void) {
-	while(1) {
 
-	}
+    while(1){
+    reorient_and_drive();
+    }
 
     // Will never return, but the compiler complains without a return
     // statement.
-
     return 0;
 }
